eef003011bbdf08adf14fd17a936435669abf3c7
Miguel Pessoa <guelmisp@gmail.com>
2017-03-10 17:22:39 -0300

    fix alucontrol errors

--- aluControl.v
+++ aluControl.v
@@ -9,7 +9,7 @@ module aluControl(
 	input [1:0] ALUop;
 	input [5:0] Instruction;
 
-	output reg [3:0];
+	output reg [3:0] ALUcontrol;
 
 	always @ (ALUop or Instruction or ALUcontrol)
 
@@ -17,7 +17,7 @@ module aluControl(
 			2'b00: ALUcontrol <= 4'b0010; //addi
 			2'b01: ALUcontrol <= 4'b0110; //beq (sub)
 			//R-type
-			default: case(Instruction):
+			default: case(Instruction)
 						 6'b100000: ALUcontrol <= 4'b0010; //add
 						 6'b100010: ALUcontrol <= 4'b0110; //sub
 						 6'b100100: ALUcontrol <= 4'b0000; //and
