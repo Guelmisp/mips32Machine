d826250a9fe33f46cf289902ce7451e75110395a
Matheus Ikeda <matheus.ikeda10@gmail.com>
2017-03-13 12:55:36 -0300

    multiplex update

---
+++ multiplex5.v
@@ -0,0 +1,18 @@
+module multiplex5 (
+  seletor,
+  entradaA,
+  entradaB,
+  saida
+  );
+  input seletor;
+  input [4:0] entradaA;
+  input [4:0] entradaB;
+  output reg [4:0] saida;
+
+  always @(seletor, entradaA, entradaB)
+    if (seletor == 0)
+      out <= entradaA;
+    else
+      out <= entradaB;
+
+endmodule
